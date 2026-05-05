4204b3d019b629affe309cf069ea53116c6da9f7
adrian <adrianribeiroprofissional.com>
2026-05-05 17:51:57 -0300

    testando o menu

--- src/js/script.js
+++ src/js/script.js
@@ -20,11 +20,46 @@ menuSobre.addEventListener('click', () => {
         navbar.classList.add('w-0')
         navbar.classList.remove('w-70')
         
-    }else{
-        navbar.classList.add('w-70')
-        navbar.classList.remove('w-0')
-       
     }
    
-
+})
+let menuSkils = document.querySelector('#menuSkils')
+menuSkils.addEventListener('click', () => {
+    let navbar = document.querySelector('#navbar')
+    if(navbar.classList.contains('w-70')){
+        navbar.classList.add('w-0')
+        navbar.classList.remove('w-70')
+        
+    }
+   
+})
+let menuContato = document.querySelector('#menuContato')
+menuContato.addEventListener('click', () => {
+    let navbar = document.querySelector('#navbar')
+    if(navbar.classList.contains('w-70')){
+        navbar.classList.add('w-0')
+        navbar.classList.remove('w-70')
+        
+    }
+   
+})
+let menuProjetos = document.querySelector('#menuProjetos')
+menuProjetos.addEventListener('click', () => {
+    let navbar = document.querySelector('#navbar')
+    if(navbar.classList.contains('w-70')){
+        navbar.classList.add('w-0')
+        navbar.classList.remove('w-70')
+        
+    }
+   
+})
+let menuDepoimentos = document.querySelector('#menuDepoimentos')
+menuDepoimentos.addEventListener('click', () => {
+    let navbar = document.querySelector('#navbar')
+    if(navbar.classList.contains('w-70')){
+        navbar.classList.add('w-0')
+        navbar.classList.remove('w-70')
+        
+    }
+   
 })
